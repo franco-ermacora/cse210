177263d0f25622435d4b03dd9e5f7316ee27abd7
franco-ermacora <ermacora.fran@gmail.com>
2023-04-24 23:48:12 -0300

    C# ejercicio 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int finish = 99999999;
+        
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a number (0 to quit): ");
+        while (finish != 0)
+        {
+            Console.Write("Enter a number: ");
+
+            string userEntry = Console.ReadLine();
+            finish = int.Parse(userEntry);
+            
+            // Only add the number to the list if it is not 0
+            if (finish != 0)
+            {
+                numbers.Add(finish);
+            }
+          
+        }
+    
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+
+
+        
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
