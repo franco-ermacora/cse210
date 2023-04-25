5f7b7be9b69bfd2050123db70bba6619f4e0a083
franco-ermacora <ermacora.fran@gmail.com>
2023-04-25 13:42:59 -0300

    C# ejercicio 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string name = UserName();
+        int favorite = UserNumber();
+
+        int squ = UserSquare(favorite);
+
+        DisplayResult(name, squ);
     }
-}
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int UserSquare(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+}   
