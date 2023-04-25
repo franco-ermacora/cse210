ef4cbd9131d71b7b19d9482935b4e6fc75a8d104
franco-ermacora <ermacora.fran@gmail.com>
2023-04-24 21:29:49 -0300

    C# ejercicio 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int numbermagic = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+        while (guess != numbermagic)
+            {
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess > numbermagic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess < numbermagic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess == numbermagic)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
     }
 }
